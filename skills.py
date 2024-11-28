7f6b8379132aa19884f6ab4c55f6a7fcfda3b247
mihski <korkinmih2013@yandex.ru>
2024-11-28 23:42:01 +0300

    weathrer

--- skills.py
+++ skills.py
@@ -1,4 +1,5 @@
 import os,sys,webbrowser,requests,pyttsx3,subprocess
+from pprint import pprint
 
 #иницилизация звук. движка:
 
@@ -16,9 +17,31 @@ def vpn():
 def offpc():
     os.system("shutdoun /s")    
 
-def weather():
-    print()
 
+def weather():
+    open_weather_token = "9d215a832d490c71ce1544ee58aaf85e"
+    city = "bon"
+    code_smile = {'Clouds': "облачно \U00002601",
+                    'Clear': "ясно \U00002600",
+                    "Snow": "снег \U00001f328",
+                    "Raine": "дождь \U00002614"
+                    }
+    try:
+        r = requests.get(
+            f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}&units=metric")
+        data = r.json()
+        
+
+        city = data["name"]
+        temperature = data["main"]["temp"]
+        wind = data["wind"]["speed"]
+        wind_gradient = data["wind"]["deg"]
+        pprint(temperature,wind)
+
+    except Exception as ex:
+        print(ex)
+        print("проверте назание города")
+        
 def browser():
     print()
     webbrowser.open("https://www.youtube.com",new=2)
