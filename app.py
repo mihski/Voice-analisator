8434c6f5ba181e5fd6a71a4e32dc432922d93b90
mihski <korkinmih2013@yandex.ru>
2024-12-15 23:01:33 +0300

    add порог совпадения

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import words
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 from skills import *
+import openai
  
 q= queue.Queue()  
 device = sd.default.device = 1, 3
@@ -38,6 +39,26 @@ def recognize(data,vectorizer,clf):
     #получаем вектор полученного текста
     #сравниваем с вариантами, получая наиболее подходящий ответ
     text_vector = vectorizer.transform([data]).toarray()[0]
+
+    # Предсказание вероятностей принадлежности к каждому классу
+    predicted_probabilities = clf.predict_proba(text_vector)
+
+    # Задание порога совпадения
+    threshold = 0.2
+
+    # Поиск наибольшей вероятности и выбор ответа, если он превышает порог
+    max_probability = max(predicted_probabilities[0])
+    print(max_probability)
+
+    if max_probability >= threshold:
+        answer = clf.classes_[predicted_probabilities[0].argmax()]
+    else:
+        speaker("Команда не распознана")
+        return
+
+
+
+
     answer = clf.predict([text_vector])[0]
     print(f"answer  {answer}")
 
